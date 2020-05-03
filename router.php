6e8bc7917ba86dbf34cf5f47f1c4dd0d41cad9fb
obedkristiaji <57378618+obedkristiaji@users.noreply.github.com>
2020-05-03 22:14:05 +0700

    router change (#1)
    * change router bug
    
    * changer router bug

--- router.php
+++ router.php
@@ -1,40 +1,40 @@
 <?php
 $url = $_SERVER['REDIRECT_URL'];
-$baseURL = '/scooteross2';
+$baseURL = '/scooteross';
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     switch ($url) {
-        case $baseURL . '/DataPengguna';
+        case $baseURL . '/datapengguna';
             require_once "controller/Admin.php";
             $dataPenggunaCtrl = new AdminController();
             echo $dataPenggunaCtrl->view_data_pengguna();
             break;
-        case $baseURL . '/indexAdmin';
+        case $baseURL . '/admin';
             require_once "controller/Admin.php";
             $indexAdminCtrl = new AdminController();
             echo $indexAdminCtrl->view_index_admin();
             break;
-        case $baseURL . '/indexPimpinan';
+        case $baseURL . '/pimpinan';
             require_once "controller/Pimpinan.php";
             $indexPimpinanCtrl = new PimpinanController();
             echo $indexPimpinanCtrl->view_index_pimpinan();
             break;
-        case $baseURL . '/indexOperator';
+        case $baseURL . '/operator';
             require_once "controller/Operator.php";
             $indexOperatorCtrl = new OperatorController();
             echo $indexOperatorCtrl->view_index_operator();
             break;
-        case $baseURL . '/DataScooterAdmin';
+        case $baseURL . '/scooteradmin';
             require_once "controller/Admin.php";
             $dataScooterCtrl = new AdminController();
             echo $dataScooterCtrl->view_data_scooter();
             break;
-        case $baseURL . '/DataScooterPimpinan';
+        case $baseURL . '/scooterpimpinan';
             require_once "controller/Pimpinan.php";
             $dataScooterCtrl2 = new PimpinanController();
             echo $dataScooterCtrl2->view_data_scooter();
             break;
-        case $baseURL . '/DataPenyewa';
+        case $baseURL . '/datapenyewa';
             require_once "controller/Operator.php";
             $dataPenyewaCtrl = new OperatorController();
             echo $dataPenyewaCtrl->view_data_penyewa();
