b30fd4d153bc3f00067f96d506d287b0ef95a5f4
FlorenthiaKezia <64560923+FlorenthiaKezia@users.noreply.github.com>
2020-05-04 08:44:11 +0700

    Add files via upload

---
+++ view/Login.php
@@ -0,0 +1,17 @@
+<div class="login">
+    <form mtehod="GET" action="login">
+        <table>
+            <tr>
+                <td>Username : </td>
+                <td><input type="text" name="username"></td>
+            </tr>
+            <tr>
+                <td>Password : </td>
+                <td><input type="number" name="password"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" placeholder="Login"></td>
+            </tr>
+        </table>
+    </form>
+</div>
