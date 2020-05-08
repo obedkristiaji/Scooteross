ebec166e9cfdb67b95735e969557d6e92f560dd1
unknown <florent.kezia@gmail.com>
2020-05-08 20:33:33 +0700

    pagination button fafa

--- view/Operator/DataPenyewa.php
+++ view/Operator/DataPenyewa.php
@@ -25,7 +25,15 @@
         </div>
     <div class="flex-footer">
         <div class="right-footer">
-            <!-- pagination button -->
+            <form method="GET">
+                <?php
+                if ($_SESSION['pageCount'] > 1) {
+                    echo '<button class="btn" name="prev"><i class="fa fa-angle-left"></i></button>';
+                    echo ' ' . $_SESSION['i'] . ' ';
+                    echo '<button class="btn" name="next"><i class="fa fa-angle-right"></i></button>';
+                }
+                ?>
+            </form>
         </div>
     </div>
 </div>
