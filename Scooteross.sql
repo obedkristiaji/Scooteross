4712f4a43cfdb8cbe036e59f6fe72aa66ae16c98
unknown <florent.kezia@gmail.com>
2020-05-08 17:15:10 +0700

    perubahan sql

--- Scooteross.sql
+++ Scooteross.sql
@@ -25,74 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `noKTP` varchar(16) NOT NULL,
-  `Nama` varchar(50) NOT NULL,
-  `Alamat` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `idKec` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`noKTP`, `Nama`, `Alamat`, `email`, `idKec`) VALUES
-('3982178934523000', 'Denny Setiadi', 'Jalan Kopo no 98 Bandung', 'denny@gmail.com', 1);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `datapengguna`
 --
 
-CREATE TABLE `datapengguna` (
+CREATE TABLE `datapengguna`(
   `id` int(11) NOT NULL,
   `NamaPengguna` varchar(150) NOT NULL,
   `Alamat` varchar(150) NOT NULL,
   `Role` varchar(50) NOT NULL,
-  `KTP` varchar(50) NOT NULL
+  `KTP` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `idKel` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Dumping data for table `datapengguna`
 --
 
-INSERT INTO `datapengguna` (`id`, `NamaPengguna`, `Alamat`, `Role`, `KTP`) VALUES
-(1, 'Vivian Cecilia', 'Jalan Kembar no 9 Bandung', 'Operator', '3509749254000000'),
-(2, 'Kimberly Natalia', 'Jalan Astana Anyar no 12 Bandung', 'Operator', '3309074509000010'),
-(3, 'Antony Budiman', 'Jalan Sukajadi no 112 Bandung', 'Pimpinan Taman', '3457892345800030'),
-(4, 'Denny Setiadi', 'Jalan Kopo no 98 Bandung', 'Admin', '3982178934523000');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `datapenyewa`
---
-
-CREATE TABLE `datapenyewa` (
-  `NoKTP` varchar(50) NOT NULL,
-  `NamaPenyewa` varchar(150) NOT NULL,
-  `AlamatPenyewa` varchar(150) NOT NULL,
-  `NoScooter` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `datapenyewa`
---
-
-INSERT INTO `datapenyewa` (`NoKTP`, `NamaPenyewa`, `AlamatPenyewa`, `NoScooter`) VALUES
-('3579762987600020', 'Rani', 'Jalan Mekar Laksana no 11 Bandung', 1),
-('3928982789862000', 'Keane', 'Jalan Merdeka no 5 Bandung', 2),
-('3542349800002320', 'Britney', 'Jalan Anggrek no 7 Bandung', 3),
-('3579762987600020', 'Rani', 'Jalan Mekar Laksana no 11 Bandung', 3),
-('3928982789862000', 'Keane', 'Jalan Merdeka no 5 Bandung', 3),
-('3928982789862000', 'Keane', 'Jalan Merdeka no 5 Bandung', 2);
-
--- --------------------------------------------------------
+INSERT INTO `datapengguna` (`id`, `NamaPengguna`, `Alamat`, `Role`, `KTP`, `email`, `idKel`) VALUES
+(1, 'Vivian Cecilia', 'Jalan Kembar no 9 Bandung', 'Operator', '3509749254000000', 'VivianCel@gmail.com', '6'),
+(2, 'Kimberly Natalia', 'Jalan Astana Anyar no 12 Bandung', 'Operator', '3309074509000010', 'Kimberly@gmail.com', '7'),
+(3, 'Antony Budiman', 'Jalan Sukajadi no 112 Bandung', 'Pimpinan Taman', '3457892345800030', 'antony97@gmail.com', '2'),
+(4, 'Denny Setiadi', 'Jalan Kopo no 98 Bandung', 'Admin', '3982178934523000', 'denny@gmail.com', '1');
 
 --
 -- Table structure for table `datascooter`
@@ -121,21 +77,22 @@ INSERT INTO `datascooter` (`NoUnik`, `Warna`, `Tarif`) VALUES
 
 CREATE TABLE `kecamatan` (
   `idKec` int(11) NOT NULL,
-  `namaKec` varchar(50) NOT NULL
+  `namaKec` varchar(50) NOT NULL,
+  `idKel` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `kecamatan`
 --
 
-INSERT INTO `kecamatan` (`idKec`, `namaKec`) VALUES
-(1, 'Bojongloa Kaler'),
-(2, 'Sukajadi'),
-(3, 'Bojongloa Kidul'),
-(4, 'Sumur'),
-(5, 'Bandung Wetan'),
-(6, 'Regol'),
-(7, 'Astana Anyar');
+INSERT INTO `kecamatan` (`idKec`, `namaKec`, `idKel`) VALUES
+(1, 'Bojongloa Kaler',1),
+(2, 'Sukajadi', 2),
+(3, 'Bojongloa Kidul', 3),
+(4, 'Sumur', 4),
+(5, 'Bandung Wetan', 5),
+(6, 'Regol', 6),
+(7, 'Astana Anyar', 7);
 
 -- --------------------------------------------------------
 
@@ -145,22 +102,21 @@ INSERT INTO `kecamatan` (`idKec`, `namaKec`) VALUES
 
 CREATE TABLE `kelurahan` (
   `idKel` int(11) NOT NULL,
-  `namaKel` varchar(50) NOT NULL,
-  `idKec` int(11) NOT NULL
+  `namaKel` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `kelurahan`
 --
 
-INSERT INTO `kelurahan` (`idKel`, `namaKel`, `idKec`) VALUES
-(1, 'Kopo', 1),
-(2, 'Sukagalih', 2),
-(3, 'Mekarwangi', 3),
-(4, 'Babakan Ciamis', 4),
-(5, 'Cihapit', 5),
-(6, 'Cigereleng', 6),
-(7, 'Karanganyar', 7);
+INSERT INTO `kelurahan` (`idKel`, `namaKel`) VALUES
+(1, 'Kopo'),
+(2, 'Sukagalih'),
+(3, 'Mekarwangi'),
+(4, 'Babakan Ciamis'),
+(5, 'Cihapit'),
+(6, 'Cigereleng'),
+(7, 'Karanganyar');
 
 -- --------------------------------------------------------
 
@@ -192,7 +148,7 @@ INSERT INTO `memiliki` (`noUnik`, `noTransaksi`) VALUES
 --
 
 CREATE TABLE `mengedit` (
-  `noKTP` varchar(16) NOT NULL,
+  `noKTP` varchar(16) NOT NULL, -- no KTP pengguna yang memiliki role admin
   `noUnik` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -210,7 +166,7 @@ INSERT INTO `mengedit` (`noKTP`, `noUnik`) VALUES
 --
 
 CREATE TABLE `menyewakan` (
-  `noKTP` varchar(16) NOT NULL,
+  `noKTP` varchar(16) NOT NULL, -- no KTP pengguna yang memiliki role petugas / operator
   `noUnik` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -229,68 +185,25 @@ INSERT INTO `menyewakan` (`noKTP`, `noUnik`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `penyewa`
+-- Table structure for table `datapenyewa`
 --
 
-CREATE TABLE `penyewa` (
+CREATE TABLE `datapenyewa` (
   `NoKTP` varchar(16) NOT NULL,
   `NamaPenyewa` varchar(50) NOT NULL,
   `AlamatPenyewa` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `idKec` int(11) NOT NULL
+  `idKel` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `penyewa`
+-- Dumping data for table `datapenyewa`
 --
 
-INSERT INTO `penyewa` (`NoKTP`, `NamaPenyewa`, `AlamatPenyewa`, `email`, `idKec`) VALUES
+INSERT INTO `datapenyewa` (`NoKTP`, `NamaPenyewa`, `AlamatPenyewa`, `email`, `idKel`) VALUES
 ('3542349800002320', 'Britney', 'Jalan Anggrek no 7 Bandung', 'Britney@gmail.com', 5),
 ('3579762987600020', 'Rani', 'Jalan Mekar Laksana no 11 Bandung', 'Rani@gmail.com', 3),
 ('3928982789862000', 'Keane', 'Jalan Merdeka no 5 Bandung', 'Keane@gmail.com', 4);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `petugas`
---
-
-CREATE TABLE `petugas` (
-  `noKTP` varchar(16) NOT NULL,
-  `Nama` varchar(50) NOT NULL,
-  `Alamat` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `idKec` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `petugas`
---
-
-INSERT INTO `petugas` (`noKTP`, `Nama`, `Alamat`, `email`, `idKec`) VALUES
-('3309074509000010', 'Kimberly Natalia', 'Jalan Astana Anyar no 12 Bandung', 'Kimberly@gmail.com', 7),
-('3509749254000000', 'Vivian Cecilia', 'Jalan Kembar no 9 Bandung', 'VivianCel@gmail.com', 6);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pimpinan`
---
-
-CREATE TABLE `pimpinan` (
-  `noKTP` varchar(16) NOT NULL,
-  `Nama` varchar(50) NOT NULL,
-  `Alamat` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `idKec` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `pimpinan`
---
-
-INSERT INTO `pimpinan` (`noKTP`, `Nama`, `Alamat`, `email`, `idKec`) VALUES
-('3457892345800030', 'Antony Budiman', 'Jalan Sukajadi no 112 Bandung', 'antony97@gmail.com', 2);
 
 -- --------------------------------------------------------
 
@@ -301,7 +214,7 @@ INSERT INTO `pimpinan` (`noKTP`, `Nama`, `Alamat`, `email`, `idKec`) VALUES
 CREATE TABLE `transaksipengembalian` (
   `noTransaksi` int(11) NOT NULL,
   `waktu_pengembalian` datetime NOT NULL,
-  `noKTP` varchar(16) NOT NULL
+  `noKTP` varchar(16) NOT NULL -- no KTP penyewa
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -325,7 +238,7 @@ INSERT INTO `transaksipengembalian` (`noTransaksi`, `waktu_pengembalian`, `noKTP
 CREATE TABLE `transaksipenyewaan` (
   `noTransaksi` int(11) NOT NULL,
   `waktu_mulai` datetime NOT NULL,
-  `noKTP` varchar(16) NOT NULL
+  `noKTP` varchar(16) NOT NULL -- no KTP penyewa
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -345,17 +258,11 @@ INSERT INTO `transaksipenyewaan` (`noTransaksi`, `waktu_mulai`, `noKTP`) VALUES
 --
 
 --
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`noKTP`),
-  ADD KEY `fk_kecAdmin` (`idKec`);
-
---
 -- Indexes for table `datapengguna`
 --
 ALTER TABLE `datapengguna`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`KTP`),
+  ADD KEY `fk_kelPengguna` (`idKel`);
 
 --
 -- Indexes for table `datascooter`
@@ -367,15 +274,14 @@ ALTER TABLE `datascooter`
 -- Indexes for table `kecamatan`
 --
 ALTER TABLE `kecamatan`
-  ADD PRIMARY KEY (`idKec`);
+  ADD PRIMARY KEY (`idKec`),
+  ADD KEY `fk_kelKec` (`idKel`);
 
 --
 -- Indexes for table `kelurahan`
 --
 ALTER TABLE `kelurahan`
   ADD PRIMARY KEY (`idKel`),
-  ADD UNIQUE KEY `idKec` (`idKec`),
-  ADD UNIQUE KEY `idKec_2` (`idKec`);
 
 --
 -- Indexes for table `memiliki`
@@ -388,36 +294,22 @@ ALTER TABLE `memiliki`
 -- Indexes for table `mengedit`
 --
 ALTER TABLE `mengedit`
-  ADD KEY `fk_KTPadmin` (`noKTP`),
+  ADD KEY `fk_KTPadminPengguna` (`noKTP`),
   ADD KEY `fk_scooterEdit` (`noUnik`);
 
 --
 -- Indexes for table `menyewakan`
 --
 ALTER TABLE `menyewakan`
-  ADD KEY `fk_KTPpetugas` (`noKTP`),
+  ADD KEY `fk_KTPpetugaspengguna` (`noKTP`),
   ADD KEY `fk_scooterSewa` (`noUnik`);
 
 --
--- Indexes for table `penyewa`
+-- Indexes for table `datapenyewa`
 --
-ALTER TABLE `penyewa`
+ALTER TABLE `datapenyewa`
   ADD PRIMARY KEY (`NoKTP`),
-  ADD KEY `fk_kecPenyewa` (`idKec`);
-
---
--- Indexes for table `petugas`
---
-ALTER TABLE `petugas`
-  ADD PRIMARY KEY (`noKTP`),
-  ADD KEY `fk_kecPetugas` (`idKec`);
-
---
--- Indexes for table `pimpinan`
---
-ALTER TABLE `pimpinan`
-  ADD PRIMARY KEY (`noKTP`),
-  ADD KEY `fk_kecPimpinan` (`idKec`);
+  ADD KEY `fk_kelPenyewa` (`idKel`);
 
 --
 -- Indexes for table `transaksipengembalian`
@@ -438,12 +330,6 @@ ALTER TABLE `transaksipenyewaan`
 --
 
 --
--- AUTO_INCREMENT for table `datapengguna`
---
-ALTER TABLE `datapengguna`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- AUTO_INCREMENT for table `datascooter`
 --
 ALTER TABLE `datascooter`
@@ -454,16 +340,16 @@ ALTER TABLE `datascooter`
 --
 
 --
--- Constraints for table `admin`
+-- Constraints for table `datapengguna`
 --
-ALTER TABLE `admin`
-  ADD CONSTRAINT `fk_kecAdmin` FOREIGN KEY (`idKec`) REFERENCES `kecamatan` (`idKec`);
+ALTER TABLE `datapengguna`
+  ADD CONSTRAINT `fk_kelPengguna` FOREIGN KEY (`idKel`) REFERENCES `kelurahan` (`idKel`);
 
 --
 -- Constraints for table `kelurahan`
 --
-ALTER TABLE `kelurahan`
-  ADD CONSTRAINT `fk_kec` FOREIGN KEY (`idKec`) REFERENCES `kecamatan` (`idKec`);
+ALTER TABLE `kecamatan`
+  ADD CONSTRAINT `fk_kec` FOREIGN KEY (`idKel`) REFERENCES `kelurahan` (`idKel`);
 
 --
 -- Constraints for table `memiliki`
@@ -476,46 +362,34 @@ ALTER TABLE `memiliki`
 --
 -- Constraints for table `mengedit`
 --
-ALTER TABLE `mengedit`
-  ADD CONSTRAINT `fk_KTPadmin` FOREIGN KEY (`noKTP`) REFERENCES `admin` (`noKTP`),
+ALTER TABLE `mengedit` -- untuk pengguna yang memiliki role admin
+  ADD CONSTRAINT `fk_KTPadminPengguna` FOREIGN KEY (`noKTP`) REFERENCES `datapengguna` (`noKTP`),
   ADD CONSTRAINT `fk_scooterEdit` FOREIGN KEY (`noUnik`) REFERENCES `datascooter` (`NoUnik`);
 
 --
 -- Constraints for table `menyewakan`
 --
-ALTER TABLE `menyewakan`
-  ADD CONSTRAINT `fk_KTPpetugas` FOREIGN KEY (`noKTP`) REFERENCES `petugas` (`noKTP`),
+ALTER TABLE `menyewakan` -- untuk pengguna yang memiliki role petugas / operator
+  ADD CONSTRAINT `fk_KTPpetugasPengguna` FOREIGN KEY (`noKTP`) REFERENCES `datapengguna` (`noKTP`),
   ADD CONSTRAINT `fk_scooterSewa` FOREIGN KEY (`noUnik`) REFERENCES `datascooter` (`NoUnik`);
 
 --
--- Constraints for table `penyewa`
+-- Constraints for table `datapenyewa`
 --
-ALTER TABLE `penyewa`
-  ADD CONSTRAINT `fk_kecPenyewa` FOREIGN KEY (`idKec`) REFERENCES `kecamatan` (`idKec`);
-
---
--- Constraints for table `petugas`
---
-ALTER TABLE `petugas`
-  ADD CONSTRAINT `fk_kecPetugas` FOREIGN KEY (`idKec`) REFERENCES `kecamatan` (`idKec`);
-
---
--- Constraints for table `pimpinan`
---
-ALTER TABLE `pimpinan`
-  ADD CONSTRAINT `fk_kecPimpinan` FOREIGN KEY (`idKec`) REFERENCES `kecamatan` (`idKec`);
+ALTER TABLE `datapenyewa`
+  ADD CONSTRAINT `fk_kecPenyewa` FOREIGN KEY (`idKel`) REFERENCES `kelurahan` (`idKel`);
 
 --
 -- Constraints for table `transaksipengembalian`
 --
 ALTER TABLE `transaksipengembalian`
-  ADD CONSTRAINT `fk_KTPbalik` FOREIGN KEY (`noKTP`) REFERENCES `penyewa` (`NoKTP`);
+  ADD CONSTRAINT `fk_KTPbalik` FOREIGN KEY (`noKTP`) REFERENCES `datapenyewa` (`NoKTP`);
 
 --
 -- Constraints for table `transaksipenyewaan`
 --
 ALTER TABLE `transaksipenyewaan`
-  ADD CONSTRAINT `fk_KTPsewa` FOREIGN KEY (`noKTP`) REFERENCES `penyewa` (`NoKTP`);
+  ADD CONSTRAINT `fk_KTPsewa` FOREIGN KEY (`noKTP`) REFERENCES `datapenyewa` (`NoKTP`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
