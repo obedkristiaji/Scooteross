412adf22622e6811915229539c10ee65cd1f7e92
FlorenthiaKezia <64560923+FlorenthiaKezia@users.noreply.github.com>
2020-05-10 12:58:45 +0700

    Florenthia kezia (#46)
    * perubahan home utama
    
    * benerin router delete tampilan index sblmnya

--- router.php
+++ router.php
@@ -6,12 +6,6 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     switch ($url) {
-        // home page
-        case $baseURL . '/index';
-            require_once "controller/HomeController.php";
-            $homeCtrl = new HomeController();
-            echo $homeCtrl->view_home();
-            break;
         // data pengguna admin
         case $baseURL . '/data-pengguna';
             require_once "controller/AdminController.php";
@@ -178,7 +172,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
             header('Location: pelunasan-transaksi');
             break;
         // view home utama 
-        case $baseURL . '/home-utama' :
+        case $baseURL . '/index' :
             require_once "Controller/HomeController.php";
             $homeCtrl = new HomeController();
             echo $homeCtrl->view_home_utama();
--- view/home/index.php
+++
@@ -1,5 +0,0 @@
-    <div class="home-container">
-        <button class="item1">Admin</button>
-        <button class="item2">Pimpinan Taman</button>
-        <button class="item3">Operator</button>
-    </div>
